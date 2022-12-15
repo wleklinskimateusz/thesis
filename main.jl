850fb9c11100b267bca35a36198889eb314183fe
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2022-12-15 22:45:09 +0100

    add orthogonality

--- main.jl
+++ main.jl
@@ -4,6 +4,8 @@ include("matrices.jl")
 include("net.jl")
 include("psi.jl")
 
+
+
 using LinearAlgebra
 using Plots
 
@@ -14,7 +16,7 @@ function get_Energy(dx::Float64, a::Float64, l::Float64, v0::Float64)::Vector{Fl
     V_osc::Matrix{Float64} = get_parabolic_potential_matrix(centers, a)
     V_gauss::Matrix{Float64} = get_gauss_potential_matrix(centers, v0, a, l)
     H::Matrix{Float64} = K + V_osc + V_gauss
-    E::Vector{Float64}, c::Matrix{Float64} = eigen(H, S)
+    E::Vector{Float64}, c::Matrix{Float64} = eigen(Hermitian(H), Hermitian(S))
     return E
 end
 
@@ -31,7 +33,7 @@ end
 function calculate_ψ(dx::Float64, a::Float64, l::Float64, v0::Float64)
     centers::Vector{Float64} = generate_centers(dx)
     H, V, S = generate_matrices(centers, a, l, v0)
-    E, c::Matrix{Float64} = eigen(H, S)
+    E, c::Matrix{Float64} = eigen(Hermitian(H), Hermitian(S))
     return E, c, V
 end
 
@@ -102,27 +104,75 @@ function get_next_c_element(c_prev::Vector{Float64}, d::Matrix{Float64}, E0::Vec
     return (1 - E0[l] * α) * c_prev[l] - α * Hz
 end
 
-function iterate(δx::Float64, α::Float64, l::Float64, v0::Float64, state::Int, filename::String)
+function normalise_eigenvector(c::Vector{Float64}, S::Matrix{Float64})::Vector{Float64}
+    norm = c' * S * c
+    return c / sqrt(norm)
+end
+
+function orthonormalise_eigenvector(c::Vector{Float64}, ci::Vector{Float64})
+    r = c' * ci
+    return c - r * ci
+end
+
+function iterate(δx::Float64, α::Float64, l::Float64, v0::Float64, state::Int, filename::String, prev_c=nothing)
+    if (state > 1 && prev_c === nothing)
+        error("For excited states you have to pass previous values for c")
+    end
+    create_dir("output/frames")
     centers = generate_centers(δx)
     E0, d, V = calculate_ψ(δx, α, l, 0.0) # eigenstates for V0 = 0
     H, _, S = generate_matrices(centers, α, l, v0)
-    E, c_all = eigen(H, S)
-    c::Vector{Float64} = c_all[:, state]
+    E, c_all = eigen(Hermitian(H), Hermitian(S))
+    c::Vector{Float64} = d[:, state]
+    println(c' * S * c)
     net = generate_net(l)
     anim = Animation()
-    for t in 1:10000000
-        ψ = zeros(length(net))
-        for (i, center) in enumerate(centers)
-            ψ += generate_ψ_element(net, c[i], center, α)
-            c[i] = get_next_c_element(c, d, E0, α, H, i)
+    for t in 1:1000000000
+        new_c = zeros(length(c))
+        for i in 1:N
+            new_c[i] = get_next_c_element(c, d, E0, α, H, i)
         end
-        ψ = normalise(net, ψ)
-        if (t % 100000 == 0)
+        if (state > 1)
+            for ci in prev_c
+                new_c = orthonormalise_eigenvector(new_c, ci)
+            end
+        end
+        c = normalise_eigenvector(new_c, S)
+        if (t % 10000000 == 0)
+            ψ = zeros(length(net))
+            for (i, center) in enumerate(centers)
+                ψ += generate_ψ_element(net, c[i], center, α)
+            end
+            i::Int = trunc(t / 10000000)
+            print("$(i)%")
             p = plot(net * L0, abs2.(ψ), xlabel="x [nm]", y="probability [%]", title="t=$t", ylims=(0, 0.002))
             frame(anim, p)
+            savefig(p, "output/frames/$i.png")
         end
     end
     gif(anim, "output/$filename.gif")
+    return c
+end
+
+function save_c_to_file(c::Vector{Float64}, filename::String)
+    create_dir("output/vectors")
+    f = open("output/vectors/$filename.txt", "w")
+    for (i, element) in enumerate(c)
+        if (i == length(c))
+            write(f, "$element")
+        else
+            write(f, "$element ")
+        end
+    end
+    close(f)
+end
+
+function read_from_file(filename::String)::Vector{Float64}
+    f = open("output/vectors/$filename.txt")
+    file = read(f, String)
+    elements::Vector{Float64} = [parse(Float64, element) for element in split(file, " ")]
+    close(f)
+    return elements
 end
 
 const eps = 0.0001
@@ -133,7 +183,13 @@ function main()
     # δx, α, error = @time get_bext_params()[end]
     α, δx = 2.55 * A, 0.25 * Δx
     # @time animate_v0(δx, α, "Psi2")
-    @time iterate(δx, α, L, V0, 4, "iteration")
+    c1 = @time iterate(δx, α, L, V0, 1, "iteration")
+    save_c_to_file(c1, "c1")
+    c1 = read_from_file("c1")
+    # c2 = @time iterate(δx, α, L, V0, 2, "Psi2", [c1])
+    # save_c_to_file(c2, "c2")
+
+
 end
 
 main()
